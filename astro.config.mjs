125ab427c76e11363c6ddb04ce8ee9bf415ccd0e
jordanhoppel <jordan@modernmensfitness.com>
2026-05-07 07:42:46 -0700

    remove broken sitemap plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://modernmensfitness.com',
-  integrations: [sitemap()],
   build: {
     format: 'directory'
   }
